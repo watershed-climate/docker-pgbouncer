45ca73d9d4dc7e154f7b6486634d38bf8dbed57d
Mick Thompson <mick@watershedclimate.com>
2024-12-18 23:28:48 +0000

    copy symlinks

--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -2,6 +2,6 @@
 
 
 mkdir -p "${PGBOUNCER_CONF_DIR}/secrets/"
-cp -r /etc/secrets/* "${PGBOUNCER_CONF_DIR}/secrets/"
+cp -av /etc/secrets/* "${PGBOUNCER_CONF_DIR}/secrets/"
 
 /opt/bitnami/scripts/pgbouncer/entrypoint.sh /opt/bitnami/scripts/pgbouncer/run.sh
