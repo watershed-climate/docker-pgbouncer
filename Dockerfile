a137392551a9fc696dc43196b84629d3303d9a0d
Mick Thompson <mick@watershedclimate.com>
2024-12-18 02:12:18 +0000

    add script to image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM bitnami/pgbouncer:1.23.1
 
+COPY runwithsecrets.sh /opt/scripts/runwithsecrets.sh
 
 USER root
--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 
-COPY runwithsecrets.sh /opt/scripts/runwithsecrets.sh
-
 mkdir -p /var/data/secrets/
 cp -r /etc/secrets/ /var/data/secrets/
+chmod -R 444 /var/data/secrets/
 
 /opt/bitnami/scripts/pgbouncer/run.sh
