228a3024f925b964c3c4e944396240608123920f
Mick Thompson <mick@watershedclimate.com>
2024-12-18 02:32:27 +0000

    add entry point

--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -4,4 +4,4 @@
 mkdir -p "${PGBOUNCER_CONF_DIR}/secrets/"
 cp -r /etc/secrets/ "${PGBOUNCER_CONF_DIR}/secrets/"
 
-/opt/bitnami/scripts/pgbouncer/run.sh
+/opt/bitnami/scripts/pgbouncer/entrypoint.sh /opt/bitnami/scripts/pgbouncer/run.sh
