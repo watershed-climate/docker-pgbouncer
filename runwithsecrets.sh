b5a4c370c8c7f442658f059ac614da9e0908e15d
Mick Thompson <mick@watershedclimate.com>
2024-12-19 00:46:30 +0000

    fix

--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-mkdir -p "$/opt/bitnami/pgbouncer/conf/secrets/"
+mkdir -p "/opt/bitnami/pgbouncer/conf/secrets/"
 cp -av /etc/secrets/* "/opt/bitnami/pgbouncer/conf/secrets/"
 
 /opt/bitnami/scripts/pgbouncer/entrypoint.sh /opt/bitnami/scripts/pgbouncer/run.sh
