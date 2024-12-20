88272221fa6d464280dd832daa55cb17defe8f10
Mick Thompson <mick@watershedclimate.com>
2024-12-20 01:57:14 +0000

    deference symlinks

--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -2,7 +2,7 @@
 
 
 mkdir -p "/secrets/"
-cp -av /etc/secrets/* "/secrets/"
+cp -Lav /etc/secrets/* "/secrets/"
 chmod 444 /secrets/*
 
 /opt/bitnami/scripts/pgbouncer/entrypoint.sh /opt/bitnami/scripts/pgbouncer/run.sh
