b933919e95dd06385836979db73142a3c2a74800
Mick Thompson <mick@watershedclimate.com>
2024-12-20 20:58:18 +0000

    readlink

--- runwithsecrets.sh
+++ runwithsecrets.sh
@@ -2,7 +2,10 @@
 
 
 mkdir -p "/secrets/"
-cp -Lav /etc/secrets/* "/secrets/"
+
+for filename in /etc/secrets/*; do
+    cp $(readlink -f "$filename") "/secrets/$(basename "$filename")"
+done
 chmod 444 /secrets/*
 
 /opt/bitnami/scripts/pgbouncer/entrypoint.sh /opt/bitnami/scripts/pgbouncer/run.sh
